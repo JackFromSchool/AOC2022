25d14e07986b4253a444331a387051cc29f8b552
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 17:27:29 -0600

    Implementing New Directory System

--- Day-7/Directory.java
+++ Day-7/Directory.java
@@ -5,43 +5,41 @@ public class Directory {
    
    String name;
    String directoryIn;
-   int sumOfItems;
-   int sumOfInternalDirectories;
+   int fileSum = 0;
+   HashMap<String, Directory> directories = new HashMap<>();
+   ArrayList<String> keys = new ArrayList<>();
    ArrayList<Integer> files = new ArrayList<>();
-   ArrayList<String> internalDirectoryKeys = new ArrayList<>();
 
    public Directory(String name, String directoryIn) {
       this.name = name;
       this.directoryIn = directoryIn;
    }
 
-   public Directory(String name) {
-      this.name = name;
+   public Directory appendDirectory(String nameOfNew, ArrayList<Directory> listOfDirectories) {
+      directories.put(nameOfNew, new Directory(nameOfNew, this.name));
+      keys.add(nameOfNew);
+      listOfDirectories.add(directories.get(nameOfNew));
+      return directories.get(nameOfNew);
    }
 
-   public void putFile(int fileSize) {
-      this.files.add(fileSize);
-      this.sumOfItems += fileSize;
+   public void appendFile(int fileSize) {
+      files.add(fileSize);
+      fileSum += fileSize;
    }
 
-   public void putDirectory(String directoryKey) {
-      if(this.name.equals(directoryKey)){
-         return;
+   public int getSumOfFiles() {
+      int sum = fileSum;
+      if(directories.isEmpty()){
+         return sum;
       }
-      this.internalDirectoryKeys.add(directoryKey);
-   }
-
-   public int getSumOfFiles(ArrayList<Directory> directoryList, HashMap<String, Integer> directoryKeys) {
-      int sum = this.sumOfItems;
-      if(this.internalDirectoryKeys.isEmpty()){
-         return sum; 
+      for(String key : keys){
+         sum += directories.get(key).getSumOfFiles();
       }
-      System.out.println(this.internalDirectoryKeys);
-      for(String i : this.internalDirectoryKeys){
-         sum += directoryList.get(directoryKeys.get(i)).getSumOfFiles(directoryList, directoryKeys);
-      }
-      System.out.println("yay2");
       return sum;
+   }
+
+   public Directory getInnerDirectory(String key) {
+      return directories.get(key);
    }
    
 }
