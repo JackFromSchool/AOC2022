f4240d013e6221921fee979223d140f870e6cfeb
Jack <112974026+JackFromSchool@users.noreply.github.com>
2022-12-07 15:24:30 +0000

    in progress

--- Day-7/Directory.java
+++ Day-7/Directory.java
@@ -28,24 +28,12 @@ public class Directory {
       this.internalDirectoryKeys.add(directoryKey);
    }
 
-   public void updateSum(ArrayList<Directory> directoryList, HashMap<String, Integer> directoryKeys) {
-      int sum = 0;
-      try {
-         this.internalDirectoryKeys.size();
-      } catch (NullPointerException e) {
-         this.sumOfInternalDirectories = sum;
-         return;
-      }
-      
-      for(int i = 0; i < this.internalDirectoryKeys.size(); i++) {
-         sum += directoryList.get(directoryKeys.get(this.internalDirectoryKeys.get(i))).sumOfInternalDirectories+directoryList.get(directoryKeys.get(this.internalDirectoryKeys.get(i))).sumOfItems;
-      }
-
-      this.sumOfInternalDirectories = sum;
-   }
-
    public int getSumOfFiles(ArrayList<Directory> directoryList, HashMap<String, Integer> directoryKeys) {
-      return this.sumOfInternalDirectories+this.sumOfItems;
+      int sum = this.sumOfItems;
+      if(this.internalDirectoryKeys.isEmpty()){
+         return sum; 
+      }
+      for()
    }
    
 }
